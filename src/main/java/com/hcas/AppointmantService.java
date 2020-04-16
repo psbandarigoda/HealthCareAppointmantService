77472a0c8c141d3953ef7efdfde0e230a523838e
pasindu <bgpsandaruwan@gmail.com>
2020-04-16 09:46:37 +0530

    === Appointment Service Created === *Java *Spring boot *JAX-RS *RESTFUL *MYSQL ===================================

---
+++ src/main/java/com/hcas/AppointmantService.java
@@ -0,0 +1,16 @@
+package com.hcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AppointmantService {
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(AppointmantService.class, args);
+
+    }
+
+}
